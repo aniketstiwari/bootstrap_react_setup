72c35c6772272ec5fadfa8cb230199f7f83bc7b0
anikett-rpx <anikett_c@rpxcorp.com>
2020-10-02 12:41:17 +0530

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="text-capitialize text-center mt-2 text-danger">Hi</h1>
+      <button className="btn btn-success">Submit</button>
+
+      {/* <div className="input-group mb-3">
+        <div className="input-group-prepend">
+          <span className="input-group-text" id="basic-addon1">@</span>
+        </div>
+        <input type="text" className="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1"/>
+      </div>
+
+      <div className="input-group mb-3">
+        <input type="text" className="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2"/>
+        <div className="input-group-append">
+          <span className="input-group-text" id="basic-addon2">@example.com</span>
+        </div>
+      </div>
+
+      <label for="basic-url">Your vanity URL</label>
+      <div className="input-group mb-3">
+        <div className="input-group-prepend">
+          <span className="input-group-text" id="basic-addon3">https://example.com/users/</span>
+        </div>
+        <input type="text" className="form-control" id="basic-url" aria-describedby="basic-addon3"/>
+      </div>
+
+      <div className="input-group mb-3">
+        <div className="input-group-prepend">
+          <span className="input-group-text">$</span>
+        </div>
+        <input type="text" className="form-control" aria-label="Amount (to the nearest dollar)"/>
+        <div className="input-group-append">
+          <span className="input-group-text">.00</span>
+        </div>
+      </div>
+
+      <div className="input-group">
+        <div className="input-group-prepend">
+          <span className="input-group-text">With textarea</span>
+        </div>
+        <textarea className="form-control" aria-label="With textarea"></textarea>
+      </div> */}
+    </>
   );
 }
 
